34eaf70bbb33b2c33b2caacfa091d74396a1b76b
melbcoding <melbcoding@gmail.com>
2023-03-07 18:59:52 -0800

    added comments in models

--- flask_app/controllers/user_routes.py
+++ flask_app/controllers/user_routes.py
@@ -3,8 +3,8 @@ from flask import render_template, redirect, request, session
 from flask_app.models import user_model
 
 @app.route('/')
-def show_users():
-    return render_template('users_blogs.html', one_user= user_model.User.get_user_with_posts())
+def user_with_blogs():
+    return render_template('user_w_blogs.html', one_user= user_model.User.get_user_with_posts())
 
 @app.route('/users/new')
 def add_users():
@@ -12,6 +12,6 @@ def add_users():
 
 @app.route('/process_user', methods=['POST'])
 def save_user():
-    User.save_user(request.form)
+    user_model.User.save_user(request.form)
     return redirect('/users')
 
--- flask_app/models/user_model.py
+++ flask_app/models/user_model.py
@@ -15,6 +15,7 @@ class User:
 
     # queries with CRUD and OOP
 
+    # get all of the users from database, return them as a list of User instances
     @classmethod
     def get_all(cls):
         query='''
@@ -31,7 +32,7 @@ class User:
             
         return user_objects
     
-
+    # save a user to database
     @classmethod
     def save_user(cls, form_data):
         query= '''
@@ -41,9 +42,12 @@ class User:
                 (%(f_name)s, %(l_name)s, %(email)s);
         '''
         results= connectToMySQL(cls.my_db).query_db(query, form_data)
-
+        
+        # the results is returning the id of the record that was just created back to the route that called this method
         return results
     
+
+    # query for a user with all of their posts, return one User instance with a list of Post instances 
     @classmethod
     def get_user_with_posts(cls):
         query= '''
@@ -53,9 +57,15 @@ class User:
             WHERE users.id = 1
         '''
         results= connectToMySQL(cls.my_db).query_db(query)
+
         print(results)
+        # read your terminal, see if this query worked by adding a print statement. If it doesn't, it will say "something went wrong..." followed by the error 
+
+        # creating a User instance from the database info of the user
         one_user = cls(results[0])
         for row in results:
+
+            # parsing out the database data for the post
             post_data={
                 "id" : row['posts.id'], 
                 "user_id": row['user_id'],
@@ -64,7 +74,11 @@ class User:
                 "created_at": row['posts.created_at'],
                 "updated_at": row['posts.updated_at']
             }
+
+            # creating a post instance and appending it to the user's all_post attribute-which is an empty list.
             one_user.all_posts.append(post_model.Post(post_data))
+
+            # return this user to the route in the controller, to be used in the template
         return one_user
 
 
