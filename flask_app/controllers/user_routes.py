87ddda2efabe0fbe0ba8132d0c31eef40ccd6be8
melbcoding <melbcoding@gmail.com>
2023-03-08 20:49:27 -0800

    registration update

--- flask_app/controllers/user_routes.py
+++ flask_app/controllers/user_routes.py
@@ -3,15 +3,21 @@ from flask import render_template, redirect, request, session
 from flask_app.models import user_model
 
 @app.route('/')
-def user_with_blogs():
-    return render_template('user_w_blogs.html', one_user= user_model.User.get_user_with_posts())
+def login_reg():
+    return render_template('log_reg.html')
 
-@app.route('/users/new')
-def add_users():
-    return render_template('user_form.html')
+@app.route('/user/register', methods=['POST'])
+def register_user():
+    if not user_model.User.validate_user(request.form):
+        return redirect('/')
+    new_user= user_model.User.save_user(request.form)
+    print(new_user)
+    return redirect(f'/user/{new_user}')
 
-@app.route('/process_user', methods=['POST'])
-def save_user():
-    user_model.User.save_user(request.form)
-    return redirect('/users')
-
+@app.route('/user/<int:id>')
+def user_profile(id):
+    data_dict={
+        "user_id":id
+    }
+    logged_user =user_model.User.get_user_with_posts(data_dict)
+    return render_template('user_w_blogs.html', one_user = logged_user )
