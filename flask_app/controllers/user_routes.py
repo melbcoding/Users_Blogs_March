41a876a3072c54b3a7ab7eb43d3a54fdfb0db411
melbcoding <melbcoding@gmail.com>
2023-03-09 19:24:34 -0800

    update from lecture on 3/9

--- flask_app/controllers/user_routes.py
+++ flask_app/controllers/user_routes.py
@@ -8,16 +8,40 @@ def login_reg():
 
 @app.route('/user/register', methods=['POST'])
 def register_user():
-    if not user_model.User.validate_user(request.form):
+    if not user_model.User.validate_register(request.form):
         return redirect('/')
     new_user= user_model.User.save_user(request.form)
     print(new_user)
+    session['user_id'] = new_user
     return redirect(f'/user/{new_user}')
+
+@app.route('/user/login', methods=['POST'])
+def login_user():
+    if not user_model.User.validate_login(request.form):
+        return redirect('/')
+    email_data={
+        'email':request.form['login_email']
+    }
+    returning_user= user_model.User.get_user_by_email(email_data)
+    session['user_id']= returning_user.id
+    return redirect(f'/user/{returning_user.id}')
 
 @app.route('/user/<int:id>')
 def user_profile(id):
+    if 'user_id' not in session:
+        return redirect('/')
     data_dict={
         "user_id":id
     }
     logged_user =user_model.User.get_user_with_posts(data_dict)
     return render_template('user_w_blogs.html', one_user = logged_user )
+
+@app.route('/all_users')
+def all_users():
+    user_model.User.get_all()
+    return "look at the terminal results"
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
--- flask_app/models/user_model.py
+++ flask_app/models/user_model.py
@@ -1,8 +1,13 @@
+from flask_app import app
+
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models import post_model
 from flask import flash
+from flask_bcrypt import Bcrypt
+bcrypt= Bcrypt(app)
 import re
 EMAIL_REGEX= re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
 
 
 
@@ -13,6 +18,7 @@ class User:
         self.f_name=data['first_name']
         self.l_name=data['last_name']
         self.email=data['email']
+        self.password= data['password']
         self.created_at= data['created_at']
         self.updated_at= data['updated_at']
         self.all_posts = []
@@ -23,29 +29,39 @@ class User:
     @classmethod
     def get_all(cls):
         query='''
-            SELECT * FROM users;
+            SELECT * FROM user;
         '''
         results= connectToMySQL(cls.my_db).query_db(query)
 
         print(results)
-
-        user_objects=[]
-        for record in results:
-            one_user= cls(record)
-            user_objects.append(one_user)
-            
-        return user_objects
+        if results:
+            user_objects=[]
+            for record in results:
+                one_user= cls(record)
+                user_objects.append(one_user)
+                
+            return user_objects
+        else:
+            return None
     
     # save a user to database
     @classmethod
     def save_user(cls, form_data):
+        pw_hash = bcrypt.generate_password_hash(form_data['password'])
+        print(pw_hash)
+        user_data= {
+            'f_name': form_data['f_name'], 
+            'l_name': form_data['l_name'],
+            'email': form_data['email'],
+            'password': pw_hash
+        }
         query= '''
                 INSERT INTO users
-                (first_name, last_name, email)
+                (first_name, last_name, email, password)
                 VALUES
-                (%(f_name)s, %(l_name)s, %(email)s);
+                (%(f_name)s, %(l_name)s, %(email)s, %(password)s);
         '''
-        results= connectToMySQL(cls.my_db).query_db(query, form_data)
+        results= connectToMySQL(cls.my_db).query_db(query, user_data)
         
         # the results is returning the id of the record that was just created back to the route that called this method
         return results
@@ -58,7 +74,7 @@ class User:
             SELECT * FROM users
             LEFT JOIN posts
             ON users.id = posts.user_id
-            WHERE users.id = %(user_id)s;
+            WHERE users.id = %(user_id)s
         '''
         results= connectToMySQL(cls.my_db).query_db(query, data)
 
@@ -86,24 +102,67 @@ class User:
         return one_user
     
 
+    # get user by email, in order to check if they exist, if so they can log in
+    @classmethod
+    def get_user_by_email(cls, data):
+
+        query= '''
+            SELECT * FROM users
+            WHERE users.email = %(email)s;
+        '''
+        results= connectToMySQL(cls.my_db).query_db(query, data)
+        if results:
+            one_user = cls(results[0])
+            return one_user
+        else:
+            return False
+
+
+
+
     @staticmethod
-    def validate_user(form_data):
+    def validate_register(form_data):
         is_valid= True
+        # check if the user exists
+        data= { "email": form_data["email"]}
+        valid_user = User.get_user_by_email(data)
+
+        # registration validations
         if len(form_data['f_name']) < 2:
             flash("First Name must be atleast 2 characters", "register")
             is_valid= False
         if len(form_data['l_name']) < 2:
-            flash("Last Name must be atleast 2 characters")
+            flash("Last Name must be atleast 2 characters", "register")
             is_valid= False
         if not EMAIL_REGEX.match(form_data['email']):
-            flash("Invalid email address!")
+            flash("Invalid email address!","register")
+            is_valid=False
+        if valid_user:
+            flash("Email already in use!", "register")
             is_valid=False
         if len(form_data['password']) < 8:
-            flash("Password needs to be atleast 8 characters")
+            flash("Password needs to be atleast 8 characters","register")
             is_valid=False
         if form_data['conf_password'] != form_data['password']:
-            flash("password and confirm password must match!")
+            flash("password and confirm password must match!","register")
             is_valid=False
+
+        return is_valid
+        
+
+    @staticmethod
+    def validate_login(form_data):
+        is_valid= True
+        
+        data= { "email": form_data["login_email"]}
+        valid_user = User.get_user_by_email(data)
+        if not valid_user:
+            flash('Invalid Crendentials', "login")
+            is_valid=False
+        if valid_user:
+            if not bcrypt.check_password_hash(valid_user.password, form_data['login_password']):
+                flash('Invalid Credentials','login')
+                is_valid=False
         return is_valid
 
 
